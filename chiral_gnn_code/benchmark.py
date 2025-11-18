8d62538d97ea47dea2c75a7d57eb3c6a7ffc28e1
qingyi zhu <qingyizhu@qingyideMacBook-Air-192.local>
2025-11-18 10:42:59 +0000

    adding features

--- chiral_gnn_code/benchmark.py
+++ chiral_gnn_code/benchmark.py
@@ -1,5 +1,7 @@
 
 import argparse
+
+from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessClassifier
 import os
 from sklearn.model_selection import KFold
@@ -11,8 +13,9 @@ from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, Gradi
 import pandas as pd
 
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score, accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score, log_loss
+from sklearn.metrics import roc_auc_score, accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score,log_loss
 from matplotlib import colormaps
+
 
 
 def settings():
@@ -31,16 +34,24 @@ def settings():
                         help='Choose to use the scaler or not depending on the model')
     parser.add_argument('--save-dir', type=str, )
     parser.add_argument('--data', type=str, )
+    parser.add_argument('--features',
+                    nargs='+',
+                    choices=['atomic number', 'hybridization', 'chirality type', 'xyz'],
+                    help='Choose one or more of the available options: atomic number, hybridization, chirality type, xyz')
+
+
+
     # gather the input: model_name, scaler, fold_number
     model_name = parser.parse_args().model_name
     scaler = parser.parse_args().scaler
     fold = parser.parse_args().fold
+
     # set up the save directory
     save_dir = parser.parse_args().save_dir
     file_dir = os.path.join(save_dir, model_name, 'fold_' + str(fold))
     os.makedirs(file_dir, exist_ok=True)
     # get the data
-    X, y = build_dataset(parser.parse_args().data)
+    X, y = build_dataset(features=parser.parse_args().features, pickle_path=parser.parse_args().data)
     # cross-validation
     cv = KFold(n_splits=5, shuffle=True, random_state=3)
     train_idx, test_idx = list(cv.split(X))[fold]
@@ -74,6 +85,13 @@ def benchmark(train_X, test_X, train_y, model, scaler, fold, test_y):
     else:
         train_X, test_X = train_X, test_X
 
+    if model == GaussianProcessClassifier(random_state=3):
+        pca = PCA(n_components=50)
+        train_X = pca.fit_transform(train_X)
+        test_X = pca.transform(test_X)
+    else:
+        train_X,test_X = train_X, test_X
+
     model.fit(train_X, train_y)
     # print(model.kernel_)
     # print(model.base_estimator_.X_train_.dtype)
@@ -84,28 +102,11 @@ def benchmark(train_X, test_X, train_y, model, scaler, fold, test_y):
 
     return model_pred, model_pred_proba
 
-    # if model_name == 'rf':
-    # model = RandomForestClassifier(random_state=3)
-    # elif model_name == 'gpc':
-    # model = GaussianProcessClassifier(random_state=3)
-    # elif model_name == 'SVM':
-    # model = SVC(random_state=3)
-    # elif model_name == 'ExtraTrees':
-    # model = ExtraTreesClassifier(random_state=3)
-    # elif model_name == 'GradientBoosting':
-    # model = GradientBoostingClassifier(random_state=3)
-    # else:
-    # print("No model selected")
-
-    # idxs_train, idxs_test = train_test_split(idxs, test_size=0.2, random_state=3)
-    # test_X, test_y = X[idxs_test], y[idxs_test]
-
 #summarize the result of the model
 def benchmark_result(test_y, model_pred, model_pred_proba, model_name, file_dir):
     f1 = f1_score(test_y, model_pred)
     roc_auc = roc_auc_score(test_y, model_pred_proba[:, 1])
-    log_loss_test = log_loss (test_y, model_pred_proba[:, 1])
-
+    loss = log_loss(test_y, model_pred_proba)
 
     matrix = confusion_matrix(test_y, model_pred)
     disp = ConfusionMatrixDisplay(confusion_matrix=matrix)
@@ -114,7 +115,7 @@ def benchmark_result(test_y, model_pred, model_pred_proba, model_name, file_dir)
     plt.savefig(os.path.join(file_dir,"confusion_matrix.png"), dpi=500)
     performance_result = [{"Model": model_name, "Accuracy": accuracy_score(test_y, model_pred),
                                       'ROC AUC': roc_auc,
-                                      'F1 Score': f1, 'log loss': log_loss_test }]
+                                      'F1 Score': f1, 'test loss': loss}]
     print(performance_result)
 
 
@@ -123,82 +124,7 @@ def benchmark_result(test_y, model_pred, model_pred_proba, model_name, file_dir)
     df_result = pd.DataFrame({"true": test_y, "prediction": model_pred, "proba": model_pred_proba[:, 1]})
     df_result.to_csv(os.path.join(file_dir, "model_prediction_result.csv"))
 
-    return f1, log_loss_test
-
-
-
-# def randomforestclassification(X, y, save_dir):
-#     rf = RandomForestClassifier(
-#         n_estimators=100,
-#         random_state=42,
-#     )
-#     train_inputs, test_inputs, train_labels, test_labels = train_test_split(
-#         X, y, test_size=0.2, random_state=3,
-#         stratify=y
-#     )
-#
-#     rf.fit(train_inputs, train_labels)
-#     rf_preds = rf.predict(test_inputs)
-#     rf_pred_proba = rf.predict_proba(test_inputs)
-#
-#     df_rf_pred=pd.DataFrame({"true": test_labels, "prediction": rf_preds, "pred_proba": rf_pred_proba[:,1]})
-#     df_rf_pred.to_csv(Path(save_dir, "Random Forest predictions.csv"))
-#
-#     Random_forest_accuracy = {accuracy_score(test_labels, rf_preds)}
-#     Random_forest_roc_auc_score = {roc_auc_score(test_labels, rf_pred_proba[:,1])}
-#     Random_forest_loss={log_loss(test_labels, rf_pred_proba)}
-#     Random_forest_f1_score= {f1_score(test_labels, rf_preds)}
-#
-#     matrix = confusion_matrix(test_labels, rf_preds)
-#     disp = ConfusionMatrixDisplay(confusion_matrix=matrix)
-#     disp.plot(cmap=colormaps.get_cmap('Reds'))
-#     plt.title('Random Forest Predictions')
-#     plt.savefig(os.path.join(save_dir, "Random Forest Classifier Result.png"), dpi=300)
-#
-#     df_rf_result = pd.DataFrame([{"Model": "Random Forest",
-#                                  "Accuracy": Random_forest_accuracy,
-#                                  "ROC AUC": Random_forest_roc_auc_score,
-#                                  "Log Loss": Random_forest_loss,
-#                                  "F1 Score": Random_forest_f1_score }])
-# #save_to_csv
-#     df_rf_result.to_csv(os.path.join(save_dir, "Random forest table.csv"), index=False)
-#
-#     return Random_forest_f1_score, Random_forest_accuracy, Random_forest_loss, Random_forest_roc_auc_score, df_rf_result
-#
-#
-#
-#
-#
-# def gaussianclassification(X, y, save_dir):
-#     gpc=GaussianProcessClassifier(random_state=3)
-#     train_inputs, test_inputs, train_labels, test_labels = train_test_split(
-#         X, y, test_size=0.2, random_state=3,
-#         stratify=y
-#     )
-#     gpc.fit(train_inputs, train_labels)
-#     gpc.score(test_inputs, test_labels)
-#
-#
-#
-#     gpc_preds = gpc.predict(test_inputs)
-#     gpc_preds_proba = gpc.predict_proba(test_inputs)
-#
-#     df_gpc_pred= pd.DataFrame({"true": test_labels, "prediction": gpc_preds, "proba": gpc_preds_proba[:,1]})
-#     df_gpc_pred.to_csv(Path(save_dir, "Gaussian Process predictions.csv"))
-#
-#     Gaussian_accuracy = {accuracy_score(test_labels, gpc_preds)}
-#     Gaussian_roc_auc_score = {roc_auc_score(test_labels, gpc_preds_proba[:,1])}
-#     Gaussian_loss = {log_loss(test_labels, gpc_preds_proba)}
-#     Gaussian_f1_score = {f1_score(test_labels, gpc_preds)}
-#
-#     matrix = confusion_matrix(test_labels, gpc_preds)
-#     disp = ConfusionMatrixDisplay(confusion_matrix=matrix)
-#     disp.plot(cmap=colormaps.get_cmap('Blues'))
-#     plt.title('Gaussian Process result')
-#     plt.savefig(os.path.join(save_dir,"Gaussian Process Classifier result.png"), dpi=300)
-#
-#     return Gaussian_accuracy , Gaussian_roc_auc_score, Gaussian_loss, Gaussian_f1_score
-
+    return f1
 
 
 
--- chiral_gnn_code/dataconversion.py
+++ chiral_gnn_code/dataconversion.py
@@ -4,52 +4,99 @@ import numpy as np
 import pandas as pd
 from rdkit import Chem, DataStructs
 from rdkit.Chem import rdFingerprintGenerator as rdFP
+from torch.fx.experimental.unification.multipledispatch.conflict import consistent
+from smiles_to_geometric_data import Node_Info
 
 def build_dataset(
-    csv_path: str= './data/processed/dataset.csv',
+    features: list[str],
+    pickle_path: str= 'data/processed_data_with_xyz.pickle',
     smiles_col: str = "SMILES",
     rotation_col: str = "Rotation",
     radius: int = 3,
-    n_bits: int = 1024,
-    include_chirality: bool = True,
+    n_bits: int = 512,
 ) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Returns:
-      X: (N, n_bits) float32 fingerprint matrix
-      y: (N,) int labels (Rotation '+'->1, '-'->0)
-      df_clean: original cols + helper cols for traceability
-    Drops rows with invalid SMILES or missing/unmappable rotation.
-    """
-    df = pd.read_csv(csv_path)
 
-    # Clean raw columns
+    df = pd.read_pickle(pickle_path)
     smiles = df[smiles_col]
     rotation = df[rotation_col]
 
+    node_info = Node_Info()
+
+
     # Make labels: '+' -> 1, '-' -> 0
     y_series = rotation.map({"+": 1, "-": 0})
-    y = y_series.to_numpy(dtype=int)
+    y = y_series.to_numpy(dtype=np.float32)
 
-    # Morgan generator (bit fingerprint)
-    mgen = rdFP.GetMorganGenerator(radius=radius, fpSize=n_bits, includeChirality=include_chirality)
-    X = np.zeros((len(smiles), n_bits), dtype=np.int8)
+    # mgen = rdFP.GetMorganGenerator(radius=radius, fpSize=n_bits, includeChirality=True)
+    # X = np.zeros((len(smiles), n_bits), dtype=np.float32)
+    x =[]
 
+    features_dict = {
+        "atomic number": node_info.find_atomic_num,
+        "hybridization": node_info.find_hybridization,
+        "chirality type": node_info.find_chiral_type,
+    }
+
+
+    #the i-th row will store the corresponding mfp
 
     for i, smi in enumerate(smiles):
         mol = Chem.MolFromSmiles(smi)
-        mfp = mgen.GetFingerprint(mol)
-        DataStructs.ConvertToNumpyArray(mfp, X[i])
+        # mfp = mgen.GetFingerprint(mol)
+        xyz_mol = df.iloc[i]['xyz']
+        # DataStructs.ConvertToNumpyArray(mfp, X[i])
+
+        mol_features =[]
+        for id, atom in enumerate(mol.GetAtoms()):
+            atom_features = []
+            if 'atomic number' in features:
+                atom_number = features_dict['atomic number'](atom)
+                atom_features.append(atom_number)
+            else:
+                atom_features = atom_features
+            if 'hybridization' in features:
+                hybridization = features_dict['hybridization'](atom)
+                atom_features.append(hybridization)
+            else:
+                atom_features = atom_features
+            if 'chirality type' in features:
+                chirality_type = features_dict['chirality type'](atom)
+                atom_features.append(chirality_type)
+            else:
+                atom_features = atom_features
+            if 'xyz' in features:
+                xa, ya, za = xyz_mol[id]
+                atom_features.append(xa)
+                atom_features.append(ya)
+                atom_features.append(za)
+            else:
+                atom_features = atom_features
+
+            mol_features.append(atom_features)
+            # atom_number = node_info.find_atomic_num(atom)
+            # chirality_type = node_info.find_chiral_type(atom)
+            # hybridization = node_info.find_hybridization(atom)
+            # xa,ya,za = xyz_mol[id]
+            # mol_features.append([atom_number, chirality_type, hybridization, xa, ya, za])
+        x.append(np.array(mol_features))
+
+        max_atom = max(m.shape[0] for m in x)
+        feat_num = x[0].shape[1]
+
+        x_3d = np.array([np.pad(m,((0,max_atom-m.shape[0]),(0,0)), mode='constant') for m in x])
+        X= x_3d.reshape(x_3d.shape[0], max_atom*feat_num)
+
+
+
+
+
+
+
 
     return X, y
 
 if __name__ == "__main__":
-    X, y = build_dataset("./data/processed_data.csv")
+    X, y= build_dataset(features=['atomic number','chirality type', 'xyz'])
+    print(X)
+    # print("y:", y.shape, y.dtype, y.nbytes / 1e6)
 
-    # basic info-to test the code literally works fine
-    # print("X shape:", X.shape)
-    #print("y shape:", y.shape)
-   # print(df_clean.head())            # first few rows
-    #print("Class balance:\n", df_clean["rotation_binary"].value_counts())
-    #print(df_clean.columns.tolist())
-    # print(X)
-    #print(y)
